174501d8824bd4d6180197dcec59b5c00b9a2240
Eliot Sanford <techieeliot@gmail.com>
2021-06-21 06:46:05 -0500

    docs: add notes from Friday

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'My name is Eliot!')
+// fs.writeFileSync('notes.txt', 'My name is Eliot!')
+
+fs.appendFileSync('notes.txt', ' I am 33 years old.', ()=> {
+	if (err) throw err
+	console.log('good!')
+})
