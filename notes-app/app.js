d632647ac9bff179bb555ecaa003a98ee7a31085
Eliot Sanford <techieeliot@gmail.com>
2021-06-25 17:58:46 -0500

    feat: create validation for emails and urls strings

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,12 @@
+const validator = require('validator')
 const getNotes = require('./notes')
 
 const msg = getNotes()
 
-console.log(msg) 
+console.log(msg)
+
+console.log(validator.isEmail('eliot587@gmail.com')) 
+console.log(validator.isEmail('gmail.com')) 
+
+console.log(validator.isURL('https://techieeliot.com')) 
+console.log(validator.isURL('@techieeliot')) 
