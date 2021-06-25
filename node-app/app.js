b83e4c4706d08bf7dbbba835a91f6a76e4cb6458
Eliot Sanford <techieeliot@gmail.com>
2021-06-25 16:47:00 -0500

    complete challenge

--- node-app/app.js
+++ node-app/app.js
@@ -1,16 +1,5 @@
 const getNotes = require('./notes')
 
-// getNotes() // my initial implementation
-
 const msg = getNotes()
 
 console.log(msg) 
-// Mead's implementation
-
-// const {name, add} = require('./utils')
-
-// console.log(name)
-
-// const sum = add(1,2)
-
-// console.log(sum)
--- node-app/utils.js
+++
@@ -1,9 +0,0 @@
-console.log('utils.js')
-
-const name = 'Eliot'
-
-const add = function (a, b) {
- return a+b
-}
-
-module.exports = {name, add}
