82bad4ebf682f90a0beb8e96a7a58475e433f8a7
Eliot Sanford <techieeliot@gmail.com>
2021-06-28 09:46:27 -0500

    feat: teach selah about chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,8 +4,9 @@ const getNotes = require('./notes')
 const msg = getNotes()
 console.log(msg)
 
-console.log(chalk.inverse.bold.green('Success!'))
-console.log(chalk.inverse.bgGreen('-------------'))
+console.log(chalk.inverse.bold.green.italic('Success!'))
+console.log(chalk.inverse.blue.italic.dim('Hey hey hey') + chalk.magenta(' Selah!!!!'))
+console.log(chalk.green('-------------'))
 
 console.log(chalk.bold.red('Danger, Young Robinson!!!'))
 
@@ -13,6 +14,7 @@ const info = chalk.italic.blue;
 const error = chalk.bold.red;
 const warning = chalk.keyword('yellow');
 
+console.log(chalk.green('-------------'))
 console.log(info('Get better at node, man!'))
 console.log(error('You really messed something up, dude'))
 console.log(warning('You are pushing it buddy!'))
