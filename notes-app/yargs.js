c4d036f6b3f2b51e961c2ac5feaf028cbdf3d5bb
Eliot Sanford <techieeliot@gmail.com>
2021-08-04 09:28:09 -0400

    feat: add yargs commands add and remove

---
+++ notes-app/yargs.js
@@ -0,0 +1,43 @@
+const yargs = require('yargs')
+
+// console.log(process.argv) // not so helpful
+// node yargs.js add --title="you added me" 
+// outputs:
+//[
+// '/usr/local/bin/node',
+// '/Users/c8908620/Desktop/projects/node-course/notes-app/yargs.js',
+// 'add',
+// '--title=you added me'
+// ] again not so helpful
+
+// very helpful
+// console.log(yargs.argv) // for node yargs.js add --title="you added me" 
+// outputs:
+// { _: [ 'add' ], title: 'you added me', '$0': 'yargs.js' }
+
+// by default added in
+// node yargs.js --help
+// node yargs.js --version
+
+// Customize version numberQA
+// yargs.version('1.1.0')
+
+// for the notes you need to be able to add, remove, read, list
+
+// Create add command
+yargs.command({
+	command: 'add',
+	describe: 'add a new note ',
+	handler: function () {
+		console.log('add a note')
+	}
+})
+
+yargs.command({
+	command: 'remove',
+	describe: 'remove a note',
+	handler: function () {
+		console.log('remove a note')
+	}
+})
+
